dcae48fdb5d6638edf1384858bb5c50e2474df21
Dmitry Ledentsov <dmlled@yahoo.com>
2015-07-25 14:10:09 +0200

    build works. Added a readme

---
+++ premake5.lua
@@ -0,0 +1,21 @@
+include 'premake'
+
+make_solution 'test'
+
+defines {
+	'NOGDI',
+	'CROW_MSVC_WORKAROUND'
+}
+
+boost = assert(dofile 'premake/recipes/boost.lua')
+boost:set_defines()
+boost:set_includedirs()
+boost:set_libdirs()
+
+includedirs {
+	'deps/crow/include',
+	'deps/crow/amalgamate',
+}
+
+make_console_app('example_with_all', { 'src/example_with_all.cpp' })
+use_standard('c++11')
